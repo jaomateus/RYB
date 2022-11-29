73ea1a4ab14c86803714be49d2c474c223d723e7
patriz gonzalez <patrizgonzalez@gmail.com>
2022-11-29 15:07:46 +0100

    done

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -2,4 +2,21 @@ class SitesController < ApplicationController
   def index
     @sites = Site.all
   end
+
+  def new
+    @site = Site.new
+  end
+
+  def create
+    @site = Site.new(site_params)
+    @site.user = current_user
+    @site.save
+  end
+
+  private
+
+    def site_params
+      params.require(:site).permit(:project_name, :description, :address, :project_type, :project_area)
+    end
+
 end
