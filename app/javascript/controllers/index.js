ec2eabfe7005ce78cf63d326002176994b85e253
Samuel Howard samuel.howard34@gmail.com <samuel.howard34@gmail.com>
2022-11-30 17:30:06 +0100

    fix bug

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -9,9 +9,7 @@ application.register("hello", HelloController)
 
 
 import MapController from "./map_controller"
-application.register("map", MapController
-                     
+application.register("map", MapController)
+
 import WeatherController from "./weather_controller"
 application.register("weather", WeatherController)
-
-
