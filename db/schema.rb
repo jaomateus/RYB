570777770597ed3143b3b1c69b3e4a51178cce0b
Joao Mateus <jaomateus@gmail.com>
2022-12-01 17:03:11 +0100

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_01_143811) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_01_160123) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -58,7 +58,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_01_143811) do
     t.text "medicinal_uses"
     t.text "cultivation_details"
     t.text "propagation_details"
-    t.text "image_url"
+    t.string "image_url"
   end
 
   create_table "plants_logs", force: :cascade do |t|
