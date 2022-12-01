766f43b02f53747209b394913f63be27735f1da5
patriz gonzalez <patrizgonzalez@gmail.com>
2022-12-01 15:28:12 +0100

    updated the site

--- db/schema.rb
+++ db/schema.rb
@@ -86,9 +86,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_30_161201) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "fist_name"
-    t.string "last_name"
-    t.string "location"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
