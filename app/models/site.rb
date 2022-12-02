3be8eaf32cf86fd009e25009fa49755b5124251f
Joao Mateus <jaomateus@gmail.com>
2022-12-02 10:29:52 +0100

    update site cloudinary

--- app/models/site.rb
+++ app/models/site.rb
@@ -4,6 +4,7 @@ class Site < ApplicationRecord
   has_many :plants_logs, through: :logs
   has_many :site_plants
   has_many :plants, through: :site_plants
+  has_one_attached :photo
 
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
