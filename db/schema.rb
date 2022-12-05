3458221e29150ba1f0bb418f92a035443e95cf6f
Samuel Howard samuel.howard34@gmail.com <samuel.howard34@gmail.com>
2022-12-05 10:58:54 +0100

    pull this

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_04_215117) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_05_094631) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -71,6 +71,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_04_215117) do
     t.integer "medicinal_rating"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "type"
     t.text "summary"
     t.float "width"
     t.float "height"
@@ -121,6 +122,9 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_04_215117) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "location"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
@@ -128,10 +132,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_04_215117) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "first_name"
-    t.string "last_name"
     t.string "profession"
-    t.string "location"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
