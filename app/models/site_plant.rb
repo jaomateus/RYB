37f7b33a0d9903806b2cb25f1a24bb7db3ac83c6
Joao Mateus <jaomateus@gmail.com>
2022-12-08 00:18:57 +0100

    update sitt plants identifiers

--- app/models/site_plant.rb
+++ app/models/site_plant.rb
@@ -1,6 +1,14 @@
 class SitePlant < ApplicationRecord
+  before_create :set_plant_id
+
   has_many :logs
   belongs_to :plant
   belongs_to :site
   has_one_attached :photo
+
+  private
+
+  def set_plant_id
+    self.identifier += 1
+  end
 end
