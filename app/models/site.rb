686e5b218796960860aa7be06e362ec2d1888ec2
Joao Mateus <jaomateus@gmail.com>
2022-12-07 22:08:51 +0100

    update sites index page

--- app/models/site.rb
+++ app/models/site.rb
@@ -1,8 +1,8 @@
 class Site < ApplicationRecord
   belongs_to :user
-  has_many :logs
+  has_many :logs, dependent: :destroy
   has_many :plants_logs, through: :logs
-  has_many :site_plants
+  has_many :site_plants,  dependent: :destroy
   has_many :plants, through: :site_plants
   has_one_attached :photo
 
